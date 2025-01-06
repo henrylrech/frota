5d1fdc21524134c022afe535c23f801dd6f3153d
Henry Lima Rech <henry@fullsoft.com.br>
2025-01-06 14:54:51 -0300

    refactor: config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rodio::{OutputStream, Source};
-use std::fs::File;
+use rodio::OutputStream;
 use std::io::BufReader;
+use std::time::Duration;
 use enigo::{Enigo, Mouse, Settings, Key};
 use enigo::Coordinate::Abs;
 use enigo::{Direction::{Click, Press, Release}, Keyboard};
@@ -12,56 +12,63 @@ use winapi::um::winuser::{
 };
 
 struct Config {
-    mp3_path: String,
-    ani_path: String
+    mp3: String,
+    ani: String,
+    mouse_lock_interval: Duration,
 }
 
 impl Config {
-    fn new(mp3_path: &str, ani_path: &str) -> Self {
+    fn new(mp3: &str, ani: &str, mouse_lock_interval_millis: u64) -> Self {
         Config {
-            ani_path: String::from(ani_path),
-            mp3_path: String::from(mp3_path)
+            ani: ani.to_string(),
+            mp3: mp3.to_string(),
+            mouse_lock_interval: time::Duration::from_millis(mouse_lock_interval_millis),
         }
     }
 }
 
 fn main() {
-    let config = Config::new("assets/audio.mp3", "assets/cursor.ani");
+    let config = Config::new(
+        "assets/audio.mp3",     //TODO: URL paths, download file from api
+        "assets/cursor.ani",    //TODO: URL paths, download file from api
+        10000
+    );
     
     wallpaper::set_from_url("https://i.redd.it/c7iaou1ua1b21.jpg").expect("could not change wallpaper"); // terry crews
 
     let audio_thread = thread::spawn(move || {
-        play_uepa(&config.mp3_path);
+        play_uepa(&config.mp3);
     });
 
-    let mouse_thread = thread::spawn(|| {
-        lock_mouse();
+    let mouse_thread = thread::spawn(move || {
+        lock_mouse(&config.mouse_lock_interval);
     });
 
     go_to_desktop();
-    change_cursor(&config.ani_path);
+    change_cursor(&config.ani);
     let _ = audio_thread.join();
     let _ = mouse_thread.join();
 }
 
-fn play_uepa(mp3_path: &str) {
+fn play_uepa(mp3_path: &String) {
     let (_stream, stream_handle) = OutputStream::try_default().expect("Failed to get default audio output stream");
 
-    let file = File::open(mp3_path).expect("Failed to open audio file");
+    let file = std::fs::File::open(mp3_path).expect("Failed to open audio file");
+
     let source = rodio::Decoder::new(BufReader::new(file)).expect("Failed to decode audio file");
 
-    stream_handle.play_raw(source.convert_samples()).expect("Failed to play audio");
+    stream_handle.play_raw(rodio::Source::convert_samples(source)).expect("Failed to play audio");
 
     std::thread::sleep(time::Duration::from_secs(45));
 }
 
-fn lock_mouse() {
+fn lock_mouse(lock_interval: &Duration) {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
 
     loop {
         enigo.move_mouse(960, 540, Abs).unwrap();
 
-        std::thread::sleep(time::Duration::from_millis(10000));
+        std::thread::sleep(*lock_interval);
     }
 }
 
@@ -75,9 +82,9 @@ fn go_to_desktop() {
     }
 }
 
-fn change_cursor(ani_path: &str) {
+fn change_cursor(ani_path: &String) {
     if cfg!(target_os = "windows") {
-        let wide_path: Vec<u16> = OsStr::new(ani_path).encode_wide().chain(Some(0)).collect();
+        let wide_path: Vec<u16> = OsStr::new(&ani_path).encode_wide().chain(Some(0)).collect();
 
         unsafe {
             let h_cursor = LoadCursorFromFileW(wide_path.as_ptr());
