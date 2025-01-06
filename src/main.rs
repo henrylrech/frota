88043ee50a350487bacf8770ad2b844e3aa7d031
Henry Lima Rech <henry@fullsoft.com.br>
2025-01-06 13:37:08 -0300

    feat: change cursor

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use enigo::{Enigo, Mouse, Settings, Key};
 use enigo::Coordinate::Abs;
 use enigo::{Direction::{Click, Press, Release}, Keyboard};
 use std::{thread, time};
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use winapi::um::winuser::{
+    DestroyCursor, LoadCursorFromFileW, SetSystemCursor,
+};
 
 fn main() {
     wallpaper::set_from_url("https://i.redd.it/c7iaou1ua1b21.jpg").expect("could not change wallpaper"); // terry crewss
@@ -18,6 +23,7 @@ fn main() {
     });
 
     go_to_desktop();
+    change_cursor();
     let _ = audio_thread.join();
     let _ = mouse_thread.join();
 }
@@ -39,7 +45,7 @@ fn lock_mouse() {
     loop {
         enigo.move_mouse(960, 540, Abs).unwrap();
 
-        std::thread::sleep(time::Duration::from_millis(100));
+        std::thread::sleep(time::Duration::from_millis(10000));
     }
 }
 
@@ -50,5 +56,29 @@ fn go_to_desktop() {
         let _ = enigo.key(Key::Meta, Press);
         let _ = enigo.key(Key::Unicode('d'), Click);
         let _ = enigo.key(Key::Meta, Release);
+    }
+}
+
+fn change_cursor() {
+    if cfg!(target_os = "windows") {
+        let ani_path = "assets/minion.ani";
+
+        // Convert the path to a wide string
+        let wide_path: Vec<u16> = OsStr::new(ani_path).encode_wide().chain(Some(0)).collect();
+
+        unsafe {
+            // Load the .ani file as a cursor
+            let h_cursor = LoadCursorFromFileW(wide_path.as_ptr());
+            if h_cursor.is_null() {
+                return;
+            }
+
+            // Set the cursor as the system default (e.g., the arrow cursor)
+            if SetSystemCursor(h_cursor, 32512) == 0 {
+                return;
+            }
+
+            DestroyCursor(h_cursor);
+        }
     }
 }
