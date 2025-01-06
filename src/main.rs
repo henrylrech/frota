aad5a8a8057c74177fd94a377a081295a500a08d
Henry Lima Rech <henry@fullsoft.com.br>
2025-01-06 14:11:30 -0300

    feat: add config and remove comments

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,27 @@ use winapi::um::winuser::{
     DestroyCursor, LoadCursorFromFileW, SetSystemCursor,
 };
 
-fn main() {
-    wallpaper::set_from_url("https://i.redd.it/c7iaou1ua1b21.jpg").expect("could not change wallpaper"); // terry crewss
+struct Config {
+    mp3_path: String,
+    ani_path: String
+}
 
-    let audio_thread = thread::spawn(|| {
-        play_uepa();
+impl Config {
+    fn new(mp3_path: &str, ani_path: &str) -> Self {
+        Config {
+            ani_path: String::from(ani_path),
+            mp3_path: String::from(mp3_path)
+        }
+    }
+}
+
+fn main() {
+    let config = Config::new("assets/audio.mp3", "assets/cursor.ani");
+    
+    wallpaper::set_from_url("https://i.redd.it/c7iaou1ua1b21.jpg").expect("could not change wallpaper"); // terry crews
+
+    let audio_thread = thread::spawn(move || {
+        play_uepa(&config.mp3_path);
     });
 
     let mouse_thread = thread::spawn(|| {
@@ -23,20 +39,20 @@ fn main() {
     });
 
     go_to_desktop();
-    change_cursor();
+    change_cursor(&config.ani_path);
     let _ = audio_thread.join();
     let _ = mouse_thread.join();
 }
 
-fn play_uepa() {
+fn play_uepa(mp3_path: &str) {
     let (_stream, stream_handle) = OutputStream::try_default().expect("Failed to get default audio output stream");
 
-    let file = File::open("assets/audio.mp3").expect("Failed to open audio file");
+    let file = File::open(mp3_path).expect("Failed to open audio file");
     let source = rodio::Decoder::new(BufReader::new(file)).expect("Failed to decode audio file");
 
     stream_handle.play_raw(source.convert_samples()).expect("Failed to play audio");
 
-    std::thread::sleep(time::Duration::from_secs(45)); // Adjust this duration to match the audio length
+    std::thread::sleep(time::Duration::from_secs(45));
 }
 
 fn lock_mouse() {
@@ -59,21 +75,16 @@ fn go_to_desktop() {
     }
 }
 
-fn change_cursor() {
+fn change_cursor(ani_path: &str) {
     if cfg!(target_os = "windows") {
-        let ani_path = "assets/cursor.ani";
-
-        // Convert the path to a wide string
         let wide_path: Vec<u16> = OsStr::new(ani_path).encode_wide().chain(Some(0)).collect();
 
         unsafe {
-            // Load the .ani file as a cursor
             let h_cursor = LoadCursorFromFileW(wide_path.as_ptr());
             if h_cursor.is_null() {
                 return;
             }
 
-            // Set the cursor as the system default (e.g., the arrow cursor)
             if SetSystemCursor(h_cursor, 32512) == 0 {
                 return;
             }
