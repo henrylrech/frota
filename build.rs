62266704c6cc929c7610dd6ffdd1d102cfa1edde
Henry Lima Rech <henry@fullsoft.com.br>
2025-01-06 13:45:09 -0300

    fix: only add icon to binary if file exists

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 extern crate winres;
+use std::fs;
 
 fn main() {
-  if cfg!(target_os = "windows") {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/my_icon.ico");
-    res.compile().unwrap();
-  }
+    if cfg!(target_os = "windows") {
+        let icon_path = "assets/icon.ico";
+        
+        if fs::metadata(icon_path).is_ok() {
+            let mut res = winres::WindowsResource::new();
+            res.set_icon(icon_path);
+            res.compile().unwrap();
+        }
+    }
 }
